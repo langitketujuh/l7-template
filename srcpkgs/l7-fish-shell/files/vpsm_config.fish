28d2ebf92196194147c0fcd9db5bf773fc175fde
Hervy Qurrotul Ainur Rozi <hervyqa@pm.me>
2022-03-21 16:03:41 +0700

    l7-fish-shell: update to 0.1.4_4

--- srcpkgs/l7-fish-shell/files/vpsm_config.fish
+++ srcpkgs/l7-fish-shell/files/vpsm_config.fish
@@ -2,9 +2,8 @@
 function vpsm_config
 set home (echo $HOME);
 set -Ux XBPS_DISTDIR $home/.cache/void-packages
-git clone --depth 1 git://github.com/void-linux/void-packages.git $XBPS_DISTDIR
+git clone --depth 1 https://github.com/void-linux/void-packages $XBPS_DISTDIR
 vpsm binbootstrap
 vpsm update-repo
 vpsm bootstrap-up
-echo -e "\033[1;36m[FINISH]\033[0m vpsm is up to date !"
 end
