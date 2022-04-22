12b6601edfc2628f404a456293ca713f84ad62af
Hervy Qurrotul Ainur Rozi <hervyqa@pm.me>
2022-04-22 07:56:11 +0700

    change email maintainer & remove tty-share

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 #-
-# Copyright (c) 2020-2021 Hervy Qurrotul Ainur Rozi <hervyqa@pm.me>.
+# Copyright (c) 2020-2021 Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>.
 #
 # All rights reserved.
 #
