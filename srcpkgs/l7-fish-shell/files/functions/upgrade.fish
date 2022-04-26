40a4886e70ba16d0d714dff70852ba929f5a3d98
Hervy Qurrotul Ainur Rozi <hervyqa@pm.me>
2022-04-26 15:31:19 +0700

    l7-fish: change update to upgrade

--- srcpkgs/l7-fish-shell/files/functions/upgrade.fish
+++ srcpkgs/l7-fish-shell/files/functions/upgrade.fish
@@ -1,7 +1,7 @@
 function upgrade --description 'Upgrade packages from XBPS repositories'
     if [ (id -u) -ne 0 ]
-        doas l7-tools --update
+        doas l7-tools --upgrade
     else
-        l7-tools --update
+        l7-tools --upgrade
     end
 end
