fe2c54bf2cfaa3742a1789421c9f47b8e6d89dae
Hervy Qurrotul Ainur Rozi <hervyqa@pm.me>
2022-05-21 11:30:03 +0700

    l7-fish-shell: fix flatpak support

---
+++ srcpkgs/l7-fish-shell/files/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Flatpak support
+set -l xdg_data_home $XDG_DATA_HOME ~/.local/share
+set -gx --path XDG_DATA_DIRS $xdg_data_home[1]/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
+
+for flatpakdir in ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
+    if test -d $flatpakdir
+        contains $flatpakdir $PATH; or set -a PATH $flatpakdir
+    end
+end
