2ccbce8198397f63c4538039166a94e085b3ed14
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2022-05-28 01:01:13 +0700

    merged to one main repo

--- build.sh
+++ build.sh
@@ -17,47 +17,42 @@ build_xbps(){
   # clean
   XBPS_TARGET_ARCH=$ARCH ./xbps-src clean
   # build packages
-  XBPS_HOSTDIR="$HOST_DIR" ./xbps-src -m $MASTER_DIR pkg ${f}
+  ./xbps-src -m $MASTER_DIR pkg ${f}
   # add signed by
-  XBPS_TARGET_ARCH=$ARCH xbps-rindex --privkey ~/.ssh/privkey.pem --sign --signedby "LangitKetujuh Linux" $HOST_DIR/binpkgs/main
+  XBPS_TARGET_ARCH=$ARCH xbps-rindex --sign --signedby "LangitKetujuh Linux" --privkey ~/.ssh/privkey.pem $PWD/hostdir/binpkgs/main
   # generate signature
-  XBPS_TARGET_ARCH=$ARCH xbps-rindex --privkey ~/.ssh/privkey.pem --sign-pkg $HOST_DIR/binpkgs/main/${f}*.xbps
+  XBPS_TARGET_ARCH=$ARCH xbps-rindex --sign-pkg --privkey ~/.ssh/privkey.pem $PWD/hostdir/binpkgs/main/${f}*.xbps
 }
 
-generate_repodata(){
-  XBPS_TARGET_ARCH=$ARCH xbps-rindex -a $HOST_DIR/binpkgs/main/*.xbps -f
-}
-
-if [ "$ARCH" = x86_64 ]; then
-  for f in $PACKAGES; do
-    MASTER_DIR="masterdir-x86_64"
-    HOST_DIR="$PWD/hostdir-x86_64"
-    build_xbps
-  done
-  generate_repodata
-elif [ "$ARCH" = x86_64-musl ]; then
-  for f in $PACKAGES; do
-    MASTER_DIR="masterdir-x86_64-musl"
-    HOST_DIR="$PWD/hostdir-x86_64-musl"
-    build_xbps
-  done
-  generate_repodata
-elif [ "$ARCH" = i686 ]; then
+if [ "$ARCH" = i686 ]; then
+  # build packages
   for f in $PACKAGES; do
     MASTER_DIR="masterdir-i686"
-    HOST_DIR="$PWD/hostdir-i686"
     build_xbps
   done
-  generate_repodata
-elif [ "$ARCH" = all ]; then
-  for a in x86_64 x86_64-musl i686; do
-    for f in $PACKAGES; do
-      MASTER_DIR="masterdir-${a}"
-      HOST_DIR="$PWD/hostdir-${a}"
-      build_xbps
-    done
+  # generate repodata
+  XBPS_TARGET_ARCH=$ARCH xbps-rindex -a $PWD/hostdir/binpkgs/main/*.xbps -f
+elif [ "$ARCH" = x86_64 ]; then
+  # build packages
+  for f in $PACKAGES; do
+    MASTER_DIR="masterdir-x86_64"
+    build_xbps
   done
-  generate_repodata
+  # generate repodata
+  XBPS_TARGET_ARCH=$ARCH xbps-rindex -a $PWD/hostdir/binpkgs/main/*.xbps -f
+elif [ "$ARCH" = x86_64-musl ]; then
+  # build packages
+  for f in $PACKAGES; do
+    MASTER_DIR="masterdir-x86_64-musl"
+    build_xbps
+  done
+  # moving to musl dir
+  mkdir -pv $PWD/hostdir/binpkgs/main/musl
+  for f in $(ls $PWD/hostdir/binpkgs/main/ | grep "x86_64-musl" | xargs); do
+    mv $PWD/hostdir/binpkgs/main/${f} $PWD/hostdir/binpkgs/main/musl
+  done
+  # generate repodata
+  XBPS_TARGET_ARCH=$ARCH xbps-rindex -a $PWD/hostdir/binpkgs/main/musl/*.xbps -f
 else
   echo "Architecture not support"
 fi
