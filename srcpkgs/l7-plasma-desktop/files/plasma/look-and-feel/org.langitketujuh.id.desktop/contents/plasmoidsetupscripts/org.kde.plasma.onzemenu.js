e5d95bddf5618edaca2941987d56e274b3c9f708
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2022-10-14 21:31:28 +0700

    default custom plasmoid script for langitketujuh theme

---
+++ srcpkgs/l7-plasma-desktop/files/plasma/look-and-feel/org.langitketujuh.id.desktop/contents/plasmoidsetupscripts/org.kde.plasma.onzemenu.js
@@ -0,0 +1,6 @@
+applet.currentConfigGroup = new Array("General");
+applet.writeConfig("icon","distributor-logo-langitketujuh");
+applet.writeConfig("numberRows","2");
+applet.writeConfig("useExtraRunners","false");
+applet.writeConfig("favoriteApps","preferred://browser,org.kde.dolphin.desktop,ardour6.desktop,audacity.desktop,blender.desktop,cadence.desktop,carla.desktop,net.fasterland.converseen.desktop,org.corectrl.corectrl.desktop,org.kde.digikam.desktop,displaycal.desktop,org.fontforge.FontForge.desktop,org.freecadweb.FreeCAD.desktop,gmic_qt.desktop,gimp.desktop,godot.desktop,goxel.desktop,fr.handbrake.ghb.desktop,hugin.desktop,org.inkscape.Inkscape.desktop,org.kde.kid3-qt.desktop,org.kde.kdenlive.desktop,kde.kid3-qt.desktop,org.kde.konsole.desktop,org.kde.krita.desktop,librecad.desktop,libreoffice-startcenter.desktop,lmms.desktop,org.bunkus.mkvtoolnix-gui.desktop,octoxbps.desktop,com.obsproject.Studio.desktop,io.github.OpenToonz.desktop,openscad.desktop,PBE.QOwnNotes.desktop,rawtherapee.desktop,scribus.desktop,screenkey.desktop,simplescreenrecorder.desktop,org.synfig.SynfigStudio.desktop,langitketujuh.system.upgrade.desktop");
+applet.reloadConfig();
