8c81040d2de7cf5736a1e35d46111e8b3e26533a
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2022-10-15 23:40:02 +0700

    remove kicker, kickerdash, kickoff, taskmanager js

--- srcpkgs/l7-plasma-desktop/files/plasma/look-and-feel/org.langitketujuh.unity.id.desktop/contents/plasmoidsetupscripts/org.kde.plasma.taskmanager.js
+++
@@ -1,3 +0,0 @@
-applet.currentConfigGroup = new Array("General");
-applet.writeConfig("launchers","preferred://filemanager,preferred://browser,applications:libreoffice-startcenter.desktop,applications:org.kde.kate.desktop,applications:org.kde.konsole.desktop");
-applet.reloadConfig();
