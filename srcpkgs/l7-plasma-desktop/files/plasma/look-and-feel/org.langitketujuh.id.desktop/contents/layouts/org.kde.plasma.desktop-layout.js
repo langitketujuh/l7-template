4fbea464b8d974a797395c929dad0e7ca9e9973e
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2022-10-15 20:51:48 +0700

    split load default layout

--- srcpkgs/l7-plasma-desktop/files/plasma/look-and-feel/org.langitketujuh.id.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
+++ srcpkgs/l7-plasma-desktop/files/plasma/look-and-feel/org.langitketujuh.id.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -1,6 +1,65 @@
-loadTemplate("org.kde.plasma.desktop.defaultPanel")
+var panel = new Panel
+var panelScreen = panel.screen
 
-var desktopsArray = desktopsForActivity(currentActivity());
-for( var j = 0; j < desktopsArray.length; j++) {
-    desktopsArray[j].wallpaperPlugin = 'org.kde.image';
+panel.height = 2 * Math.floor(gridUnit * 2.0 / 2)
+
+const maximumAspectRatio = 21/9;
+if (panel.formFactor === "horizontal") {
+    const geo = screenGeometry(panelScreen);
+    const maximumWidth = Math.ceil(geo.height * maximumAspectRatio);
+
+    if (geo.width > maximumWidth) {
+        panel.alignment = "center";
+        panel.minimumLength = maximumWidth;
+        panel.maximumLength = maximumWidth;
+    }
 }
+
+var kickoff = panel.addWidget("org.kde.plasma.kickoff")
+kickoff.currentConfigGroup = ["Shortcuts"]
+kickoff.writeConfig("global", "Alt+F1")
+panel.addWidget("org.kde.plasma.pager")
+panel.addWidget("org.kde.plasma.icontasks")
+panel.addWidget("org.kde.plasma.panelspacer")
+panel.addWidget("org.kde.plasma.marginsseparator")
+
+var langIds = ["as",    // Assamese
+               "bn",    // Bengali
+               "bo",    // Tibetan
+               "brx",   // Bodo
+               "doi",   // Dogri
+               "gu",    // Gujarati
+               "hi",    // Hindi
+               "ja",    // Japanese
+               "kn",    // Kannada
+               "ko",    // Korean
+               "kok",   // Konkani
+               "ks",    // Kashmiri
+               "lep",   // Lepcha
+               "mai",   // Maithili
+               "ml",    // Malayalam
+               "mni",   // Manipuri
+               "mr",    // Marathi
+               "ne",    // Nepali
+               "or",    // Odia
+               "pa",    // Punjabi
+               "sa",    // Sanskrit
+               "sat",   // Santali
+               "sd",    // Sindhi
+               "si",    // Sinhala
+               "ta",    // Tamil
+               "te",    // Telugu
+               "th",    // Thai
+               "ur",    // Urdu
+               "vi",    // Vietnamese
+               "zh_CN", // Simplified Chinese
+               "zh_TW"] // Traditional Chinese
+
+if (langIds.indexOf(languageId) != -1) {
+    panel.addWidget("org.kde.plasma.kimpanel");
+}
+
+panel.addWidget("org.kde.plasma.colorpicker")
+panel.addWidget("org.kde.plasma.systemtray")
+panel.addWidget("org.kde.plasma.digitalclock")
+
