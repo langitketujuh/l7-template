20783e9029213057a15e8890395b51e82db929c6
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2023-05-09 15:51:21 +0700

    fix build packages

--- build.sh
+++ build.sh
@@ -12,33 +12,48 @@ shift $((OPTIND - 1))
 
 [ ! -x build.sh ] && exit 0
 
-# build packages
-for a in x86_64 x86_64-musl; do
-  for p in $PACKAGES; do
-    # clean
-    XBPS_TARGET_ARCH=${a} ./xbps-src clean -m masterdir-${a}
-    # new masterdir
-    ./xbps-src -m masterdir-${a} binary-bootstrap ${a}
-    # build packages
-    ./xbps-src -m masterdir-${a} pkg ${p}
+for arch in x86_64 x86_64-musl; do
+
+  # clean masterdir
+  XBPS_TARGET_ARCH=${arch} ./xbps-src clean -m masterdir-${arch}
+
+  # new masterdir
+  ./xbps-src -m masterdir-${arch} binary-bootstrap ${arch}
+
+  # build packages xbps
+  for pkgs in $PACKAGES; do
+
+    ver_pkgs=$(grep 'version=' $PWD/srcpkgs/${pkgs}/template | sed -e 's/version=//g')
+    rev_pkgs=$(grep 'revision=' $PWD/srcpkgs/${pkgs}/template | sed -e 's/revision=//g')
+    basename_pkgs="${pkgs}-${ver_pkgs}_${rev_pkgs}.${arch}.xbps"
+
+    # build xbps
+    ./xbps-src -m masterdir-${arch} pkg ${pkgs}
+
+    if [ $arch = "x86_64-musl" ]; then
+      main_dir="main/musl"
+    else
+      main_dir="main"
+    fi
+
+    # moving to musl dir if musl pkgs
+    if [ $arch = "x86_64-musl" ]; then
+      mkdir -pv $PWD/hostdir/binpkgs/main/musl
+      for musl_pkgs in $(ls $PWD/hostdir/binpkgs/main/ | grep "x86_64-musl" | xargs); do
+        mv $PWD/hostdir/binpkgs/main/${musl_pkgs} $PWD/hostdir/binpkgs/main/musl/
+      done
+    fi
+
     # add signed by
-    XBPS_TARGET_ARCH=${a} xbps-rindex --sign --signedby "LangitKetujuh Linux" --privkey ~/.ssh/privkey.pem $PWD/hostdir/binpkgs/main
+    XBPS_TARGET_ARCH=${arch} xbps-rindex --sign --signedby "LangitKetujuh Linux" --privkey ~/.ssh/privkey.pem $PWD/hostdir/binpkgs/$main_dir/
+
     # generate signature
-    XBPS_TARGET_ARCH=${a} xbps-rindex --sign-pkg --privkey ~/.ssh/privkey.pem $PWD/hostdir/binpkgs/main/${p}*.xbps
+    XBPS_TARGET_ARCH=${arch} xbps-rindex -d --sign-pkg --privkey ~/.ssh/privkey.pem $PWD/hostdir/binpkgs/$main_dir/${basename_pkgs}
+
+    # generate x86_64 repodata
+    XBPS_TARGET_ARCH=${arch} xbps-rindex -a $PWD/hostdir/binpkgs/$main_dir/${basename_pkgs} -f
+    chmod --preserve-root 644 $PWD/hostdir/binpkgs/$main_dir/${basename_pkgs}.sig
+
   done
-  # generate repodata
-  if [ $a = "x86_64-musl" ]; then
-    # moving to musl dir
-    mkdir -pv $PWD/hostdir/binpkgs/main/musl
-    for m in $(ls $PWD/hostdir/binpkgs/main/ | grep "x86_64-musl" | xargs); do
-      mv $PWD/hostdir/binpkgs/main/${m} $PWD/hostdir/binpkgs/main/musl
-    done
-    # x86_64-musl
-    XBPS_TARGET_ARCH=${a} xbps-rindex -a $PWD/hostdir/binpkgs/main/musl/*.xbps -f
-    chmod --preserve-root 644 $PWD/hostdir/binpkgs/main/musl/${p}*.sig
-  else
-    # i686 x86_64
-    XBPS_TARGET_ARCH=${a} xbps-rindex -a $PWD/hostdir/binpkgs/main/*.xbps -f
-    chmod --preserve-root 644 $PWD/hostdir/binpkgs/main/${f}*.sig
-  fi
+
 done
