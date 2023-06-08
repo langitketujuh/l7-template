77227a01cd8af51a4963cc4692f0b2d6cf109540
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2023-06-08 10:01:56 +0700

    fish-shell: func for vpsm

--- srcpkgs/l7-fish-shell/files/functions/vpsmconf.fish
+++
@@ -1,9 +0,0 @@
-function vpsmconf --description 'Install VPSM binbootstrap'
-    set home (echo $HOME);
-    set -Ux XBPS_DISTDIR $home/.cache/void-packages
-    git clone --depth 1 https://github.com/void-linux/void-packages $XBPS_DISTDIR
-    vpsm binbootstrap
-    vpsm update-repo
-    vpsm bootstrap-up
-    echo XBPS_ALLOW_RESTRICTED=yes > $XBPS_DISTDIR/etc/conf
-end
