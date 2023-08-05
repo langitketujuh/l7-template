0942bd4b951ee0504747617131dbb385794578cb
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2023-08-05 13:25:57 +0700

    add: genrepodata for generate repodata only

---
+++ genrepodata.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+for arch in x86_64 x86_64-musl; do
+
+  if [ $arch = "x86_64-musl" ]; then
+    main_dir="main/musl"
+  else
+    main_dir="main"
+  fi
+
+  for pkgs in $(ls hostdir/binpkgs/$main_dir/ | grep "\.xbps.sig" | xargs | sed -e 's/.sig//g') ; do
+
+    # generate x86_64 repodata
+    XBPS_TARGET_ARCH=${arch} xbps-rindex -a $PWD/hostdir/binpkgs/$main_dir/${pkgs}
+    chmod --preserve-root 644 $PWD/hostdir/binpkgs/$main_dir/*.xbps.sig
+
+  done
+
+done
